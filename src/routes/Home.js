76f228069fc9ce486fab8a506af3b0e59e11a36c
finnp920 <finnp920@gmail.com>
2022-05-23 17:05:59 +0900

    #1.2 GET MOVIE Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
 const GET_MOVIES = gql`
   {
@@ -9,14 +11,53 @@ const GET_MOVIES = gql`
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
 function Home() {
-  const { loading, error, data } = useQuery(GET_MOVIES);
-  console.log(loading, error, data);
-  if (loading) return "loading";
-  if (data && data.movies) {
-    return data.movies.map((movie) => <h3>{movie.id}</h3>);
-  }
-  return <h1>Home</h1>;
+  const { loading, data } = useQuery(GET_MOVIES);
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {!loading && data.movies && data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+    </Container>
+  );
 }
 
 export default Home;
