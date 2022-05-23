215dbefe8097d1910667bdab3b436f0822d029df
finnp920 <finnp920@gmail.com>
2022-05-23 16:06:28 +0900

    #1.0 Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
