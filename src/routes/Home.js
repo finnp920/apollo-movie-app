378c9257c5d426d69a8248901a73dc6120ae5c45
finnp920 <finnp920@gmail.com>
2022-05-23 16:20:58 +0900

    #1.1 GET MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,21 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
 function Home() {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  if (loading) return "loading";
+  if (data && data.movies) {
+    return data.movies.map((movie) => <h3>{movie.id}</h3>);
+  }
   return <h1>Home</h1>;
 }
 
