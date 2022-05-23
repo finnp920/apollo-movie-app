d6f1227356289a1600a38d25b3d14ff34513b0dd
finnp920 <finnp920@gmail.com>
2022-05-23 17:24:52 +0900

    #1.3 Apollo Cache and Styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -46,6 +46,15 @@ const Loading = styled.div`
   margin-top: 10px;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 function Home() {
   const { loading, data } = useQuery(GET_MOVIES);
   return (
@@ -55,7 +64,13 @@ function Home() {
         <Subtitle>I love GraphQL</Subtitle>
       </Header>
       {loading && <Loading>Loading...</Loading>}
-      {!loading && data.movies && data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+      {!loading && data.movies && (
+        <Movies>
+          {data.movies.map((m) => (
+            <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+          ))}
+        </Movies>
+      )}
     </Container>
   );
 }
